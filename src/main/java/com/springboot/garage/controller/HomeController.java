34246798d4ad37e6f0756ab72089f80ff3122462
Malix <mayerma91@gmail.com>
2019-11-12 11:37:40 +0100

    creating templates

--- src/main/java/com/springboot/garage/controller/HomeController.java
+++ src/main/java/com/springboot/garage/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.springboot.garage.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String home() {
-		return ("<h1>Welcome</h1>");
+	public String home(Model pmodel, Principal p) {
+		return ("home");
 	}
 
 	@GetMapping("/user")
